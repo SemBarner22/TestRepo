e28668715ec76fd200ed726c5a76b13aefc7bb50
sembarner22 <ildar1001@mail.ru>
2021-03-13 19:52:15 +0300

    fsesdfefg

--- core/src/com/mygdx/game/Screens/MapScreen.java
+++ core/src/com/mygdx/game/Screens/MapScreen.java
@@ -1,8 +1,11 @@
 package com.mygdx.game.Screens;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
@@ -17,12 +20,13 @@ import com.mygdx.game.EmptyScreen;
 import com.mygdx.game.Strategy;
 
 public class MapScreen extends AbstractMechanicsScreen {
-
+    private Batch batch;
     private Strategy game;
     Texture texture;
     private OrthographicCamera gameCamera;
     private Viewport gamePort;
 
+    private ShapeRenderer sr;
     private TmxMapLoader mapLoader;
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
@@ -32,24 +36,48 @@ public class MapScreen extends AbstractMechanicsScreen {
 
     public MapScreen(Strategy strategy, int i, EmptyScreen emptyScreen) {
         super(strategy, i, emptyScreen);
+        this.game = strategy;
         texture = new Texture("badlogic.jpg");
         gameCamera = new OrthographicCamera();
         gamePort = new ScreenViewport(gameCamera);
 
         mapLoader = new TmxMapLoader();
-//        map = mapLoader.load();
+        map = mapLoader.load("main_map.tmx");
         renderer = new OrthogonalTiledMapRenderer(map);
+        renderer.setView(gameCamera);
 
         gameCamera.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
         world = new World(new Vector2(0, 0), true);
         b2dr = new Box2DDebugRenderer();
 
         BodyDef bdef = new BodyDef();
-
-
     }
 
+    @Override
+    public void render(float delta) {
+        renderer.setView(gameCamera);
+        batch = game.batch;
+        game.batch.setProjectionMatrix(gameCamera.combined);
+//        sr.setProjectionMatrix(gameCamera.combined);
+//        sr.setColor(Color.CYAN);
+        game.batch.begin();
+        renderer.render(new int[]{0, 1});
+        game.batch.end();
+    }
 
+    @Override
+    public void resize(int width, int height) {
+        gameCamera.viewportWidth = width;
+        gameCamera.viewportHeight = height;
+        gameCamera.update();
+    }
+
+    @Override
+    public void dispose() {
+        texture.dispose();
+        map.dispose();
+        batch.dispose();
+    }
 
 
 }
