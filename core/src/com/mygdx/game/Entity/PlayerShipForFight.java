6b4e7d23b61f5759fea0d7c3fca6cd2b1bb9b5b7
imka239 <ima2ima3go9@gmail.com>
2021-03-13 20:28:12 +0300

    FightScreen

--- core/src/com/mygdx/game/Entity/PlayerShipForFight.java
+++ core/src/com/mygdx/game/Entity/PlayerShipForFight.java
@@ -1,8 +1,6 @@
 package com.mygdx.game.Entity;
 
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.*;
 
 public class PlayerShipForFight {
     public World world;
@@ -17,5 +15,12 @@ public class PlayerShipForFight {
         BodyDef bdef = new BodyDef();
         bdef.position.set(32, 32);
         bdef.type = BodyDef.BodyType.DynamicBody;
+        b2body = world.createBody(bdef);
+        FixtureDef fdef = new FixtureDef();
+        CircleShape shape = new CircleShape();
+        shape.setRadius(5);
+
+        fdef.shape = shape;
+        b2body.createFixture(fdef);
     }
 }
