6ca2d056fe8842c63cf9dceb2b386ca9da2f0054
Тарас Михайлович Скаженик <skazhenik.taras@niuitmo.ru>
2021-03-13 20:09:07 +0300

    feat(android/assets): added new tiles

--- android/assets/SeaAndForest.tsx
+++ android/assets/SeaAndForest.tsx
@@ -1,4 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="SeaAndForest" tilewidth="16" tileheight="16" tilecount="323" columns="17">
  <image source="tileset_16x16_final_1.png" width="272" height="304"/>
+ <wangsets>
+  <wangset name="Новый набор плиток Вана" tile="-1"/>
+ </wangsets>
 </tileset>
