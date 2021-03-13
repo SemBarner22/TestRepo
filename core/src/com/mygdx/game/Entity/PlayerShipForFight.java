065d296723911ea97a7ebed0a1cdac2c2718f943
imka239 <ima2ima3go9@gmail.com>
2021-03-13 22:27:41 +0300

    FightScreen

--- core/src/com/mygdx/game/Entity/PlayerShipForFight.java
+++ core/src/com/mygdx/game/Entity/PlayerShipForFight.java
@@ -1,16 +1,26 @@
 package com.mygdx.game.Entity;
 
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.*;
-import com.mygdx.game.Strategy;
+import com.mygdx.game.Screens.FightScreen;
+
+import java.awt.geom.RectangularShape;
 
 public class PlayerShipForFight extends Sprite {
     public World world;
     public Body b2body;
+    private TextureRegion ship;
 
-    public PlayerShipForFight(World world) {
+    public PlayerShipForFight(World world, FightScreen fightScreen) {
+        super(fightScreen.getAtlas().findRegion("iron"));
         this.world = world;
         defineShip();
+        ship = new TextureRegion(getTexture(), 55, 1, 16, 16);
+        setBounds(0, 0, 16, 16);
+        setRegion(ship);
     }
 
     public void defineShip() {
@@ -21,8 +31,11 @@ public class PlayerShipForFight extends Sprite {
         FixtureDef fdef = new FixtureDef();
         CircleShape shape = new CircleShape();
         shape.setRadius(5);
-
         fdef.shape = shape;
         b2body.createFixture(fdef);
+    }
+
+    public void update(float dt) {
+        setPosition(b2body.getPosition().x - getWidth() / 2, b2body.getPosition().y - getHeight() / 2);
     }
 }
--- core/src/com/mygdx/game/Screens/FightScreen.java
+++ core/src/com/mygdx/game/Screens/FightScreen.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
@@ -28,6 +29,7 @@ public class FightScreen extends AbstractMechanicsScreen {
     private OrthographicCamera gameCamera;
     private Viewport gamePort;
     private FightHud hud;
+    private TextureAtlas atlas;
 
     private TmxMapLoader mapLoader;
     private TiledMap map;
@@ -40,6 +42,7 @@ public class FightScreen extends AbstractMechanicsScreen {
 
     public FightScreen(Strategy strategy, int i, EmptyScreen emptyScreen) {
         super(strategy, i, emptyScreen);
+        atlas = new TextureAtlas("ship_set.txt");
         this.game = strategy;
         gameCamera = new OrthographicCamera();
         gamePort = new StretchViewport(Strategy.V_WIDTH, Strategy.V_HEIGHT, gameCamera);
@@ -51,7 +54,7 @@ public class FightScreen extends AbstractMechanicsScreen {
         gameCamera.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
         world = new World(new Vector2(0, -10), true);
         b2dr = new Box2DDebugRenderer();
-        player = new PlayerShipForFight(world);
+        player = new PlayerShipForFight(world, this);
 
         BodyDef bdef = new BodyDef();
         PolygonShape shape = new PolygonShape();
@@ -72,28 +75,30 @@ public class FightScreen extends AbstractMechanicsScreen {
         }
     }
 
+    public TextureAtlas getAtlas() {
+        return atlas;
+    }
+
     @Override
     public void show() {
 
     }
 
     public void handleInput(float dt) {
-        if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
-            player.b2body.applyLinearImpulse(new Vector2(0, 4f), player.b2body.getWorldCenter(), true);
+        if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT) && player.b2body.getLinearVelocity().x <= 10) {
+            player.b2body.applyLinearImpulse(new Vector2(10f, 0), player.b2body.getWorldCenter(), true);
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && player.b2body.getLinearVelocity().x <= 2) {
-            player.b2body.applyLinearImpulse(new Vector2(0.1f, 0), player.b2body.getWorldCenter(), true);
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && player.b2body.getLinearVelocity().x >= -2) {
-            player.b2body.applyLinearImpulse(new Vector2(-0.1f, 0), player.b2body.getWorldCenter(), true);
+        if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT) && player.b2body.getLinearVelocity().x >= -10) {
+            player.b2body.applyLinearImpulse(new Vector2(-10f, 0), player.b2body.getWorldCenter(), true);
         }
     }
 
     public void update(float dt) {
         handleInput(dt);
         world.step(1/60f, 6, 2);
+
+        player.update(dt);
         gameCamera.position.x = player.b2body.getPosition().x;
-        gameCamera.position.y = player.b2body.getPosition().y;
         gameCamera.update();
         renderer.setView(gameCamera);
     }
@@ -101,14 +106,20 @@ public class FightScreen extends AbstractMechanicsScreen {
     @Override
     public void render(float delta) {
         update(delta);
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        renderer.render();
+        b2dr.render(world, gameCamera.combined);
+
+        game.batch.setProjectionMatrix(gameCamera.combined);
+        game.batch.begin();
+        player.draw(game.batch);
+        game.batch.end();
 
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
 
-        renderer.render();
-        b2dr.render(world, gameCamera.combined);
     }
 
     @Override
